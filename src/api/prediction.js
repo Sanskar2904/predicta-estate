edcf5c6e21e4c9a8a3663bec8d5d088d5d3741b2
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-23 19:58:03 +0530

    update request content-type to application/json

--- src/api/prediction.js
+++ src/api/prediction.js
@@ -1,24 +1,26 @@
-import qs from "qs";
 import axios from "axios";
 import { closestMatch } from "closest-match";
 
 const prediction = async (searchRequest, availableLocation, setHousePrice) => {
   const { bath, bhk, name, sqft } = searchRequest;
 
-  const { data } = await axios({
-    method: "POST",
-    url: "https://predicta-estate.onrender.com/getpredictedprice",
-    data: qs.stringify({
-      total_sqft: sqft,
-      bath: bath,
-      bhk: bhk,
-      location: closestMatch(name, availableLocation) || name,
-    }),
-  });
+  try {
+    const { data } = await axios.post(
+      "https://predicta-estate.onrender.com/getpredictedprice",
+      {
+        total_sqft: sqft,
+        bath: bath,
+        bhk: bhk,
+        location: closestMatch(name, availableLocation) || name,
+      }
+    );
 
-  console.log(data);
+    console.log(data);
 
-  setHousePrice(data.estimated_price);
+    setHousePrice(data.estimated_price);
+  } catch (error) {
+    console.error("Error predicting house price:", error);
+  }
 };
 
 export default prediction;
